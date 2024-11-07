a42f30e4c9acf16855a25a0425f10653e6e533ce
Fagun06 <mdfagun1357@gmail.com>
2024-11-07 10:32:19 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
