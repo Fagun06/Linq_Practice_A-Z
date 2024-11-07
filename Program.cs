b98fdfe44e3c281d8ff91ae566b02adf43d57676
Fagun06 <mdfagun1357@gmail.com>
2024-11-07 10:35:06 +0600

    I just simple query in list and use Max funtion

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9};
+
+            var qu = from i in list where i > 4 select i;
+
+            foreach (var i in qu)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Mathodsytex");
+
+            qu = list.Where(i => i > 4);
+
+            foreach (var i in qu)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Max value");
+
+            var mixsyntex = (from i in list select i).Max();
+            Console.WriteLine(mixsyntex.ToString());
         }
 
     }
